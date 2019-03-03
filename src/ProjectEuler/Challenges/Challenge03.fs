82ba7d7d198227184d66275e3e42f7901a22ce22
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2019-03-03 17:08:27 +0000

    03

---
+++ src/ProjectEuler/Challenges/Challenge03.fs
@@ -0,0 +1,20 @@
+module Challenge02
+open System
+
+let solution = 
+    //https://projecteuler.net/problem=3
+
+    let number = 600851475143L
+
+    let getSqrt number = number |> double |> Math.Sqrt |> int64
+
+    let isFactor number candiate = number % candiate = 0L
+
+    let isPrime number = 
+        { 2L .. (getSqrt number) }
+        |> Seq.forall(fun x -> number % x <> 0L)
+
+    { 3L .. 2L .. (getSqrt number) }
+    |> Seq.filter(fun x -> isFactor number x)
+    |> Seq.filter isPrime
+    |> Seq.max
