ea70a0eef0437b35fc2f2c623d06b9adce313f5f
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2019-04-06 16:14:07 +0100

    16

--- src/ProjectEuler/Challenges/Challenge16.fs
+++ src/ProjectEuler/Challenges/Challenge16.fs
@@ -3,19 +3,12 @@ module Challenge16
 let solution = 
     //https://projecteuler.net/problem=16
     
-    let powerNumber = 1000I
     let number = 2I
-
-    let getPower (number:bigint) (powerNumber:bigint) = 
-        (number, 1I)
-        |> Seq.unfold(fun (n, p) -> Some((n, p), (n * 2I, p + 1I))) 
-        |> Seq.takeWhile(fun (_, p) -> p <= powerNumber)
-        |> Seq.last
-        |> fst
+    let powerNumber = number**1000
 
     let getSumOfDigits (number:bigint) =
         number.ToString().ToCharArray()
         |> Seq.sumBy System.Char.GetNumericValue 
         |> int
 
-    getPower number powerNumber |> getSumOfDigits
+    getSumOfDigits powerNumber
