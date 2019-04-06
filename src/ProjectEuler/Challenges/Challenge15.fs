bb07085916e0a1d33dd00684cff7c14e9e97448b
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2019-04-06 12:07:55 +0100

    added 15 to proj

---
+++ src/ProjectEuler/Challenges/Challenge15.fs
@@ -0,0 +1,22 @@
+module Challenge15
+
+let solution = 
+    //https://projecteuler.net/problem=15
+
+    let gridSize = 2;
+
+    let routeLength = gridSize * 2
+
+    //R = Right D = Down
+
+    //Same number of R and Ds in a route
+
+    // 2 x 2 grid RRDD, RDRD, RDDR, DRRD, DRDR and DDRR;
+
+    0
+      
+    
+
+
+    
+        
