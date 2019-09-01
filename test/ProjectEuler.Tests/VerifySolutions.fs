4d32ab6ffc94ab22fe5bad25dadc476030b572b2
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2019-09-01 11:55:42 +0100

    added test project

---
+++ test/ProjectEuler.Tests/VerifySolutions.fs
@@ -0,0 +1,14 @@
+﻿namespace ProjectEuler.Tests
+
+open NUnit.Framework
+open Shouldly
+
+[<TestFixture>]
+type TestClass () =
+
+    [<Test>]
+    member this.VerifySolution1() =
+        let expected = 233168
+        let actual = Challenge01.solution
+        expected.ShouldBe(actual);
+        
